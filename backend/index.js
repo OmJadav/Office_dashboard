8364c945fbbc1f5c1730e1e2c51ef8dab163aff6
Om Jadav <omjadav04@gmail.com>
2024-04-19 15:31:25 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -10,15 +10,11 @@ const router = require("./routes/allRoutes")
 
 
 //middlewares
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
-//build folder deployment
-app.use(express.static(path.join(__dirname, "build")))
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"))
-})
+
 
 
 const port = process.env.PORT
